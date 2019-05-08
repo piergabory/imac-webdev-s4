00a4be6e5ebf1b11c7613cd411c1580b5437981c
Pierre Gabory <piergabory@gmail.com>
2019-05-08 13:50:28 +0200

    Buttons

--- src/components/card.js
+++ src/components/card.js
@@ -16,10 +16,10 @@ export default ({hero, decking = false, deckingLabel = false, discard = false, d
     </div>
     <nav className='cardButtons'>
       { discardLabel && discard && <button onclick={() => discard(hero)} className='cardButton discard destructive' title={discardLabel}>
-        <svg className='glyph' viewbox="0 0 128 128"><path d="M79.627 57l33.942 33.941L90.94 113.57 57 79.627 23.059 113.57.43 90.94 34.373 57 .43 23.059 23.06.43 57 34.373 90.941.43 113.57 23.06 79.627 57z" fill-rule="evenodd"/></svg>
+        <img className='glyph' src='../../styles/img/Button Destructive.png'></img>
       </button> }
       { deckingLabel && decking && <button onclick={() => decking(hero)} className='cardButton decking additive' title={deckingLabel}>
-        <svg className='glyph' viewbox="0 0 128 128"><path fill-rule="evenodd" d="M80.51 48h48v32h-48v48h-32V80h-48V48h48V0h32v48z"/></svg>
+        <img className='glyph' src='../../styles/img/Button Additive.png'></img>
       </button> }
     </nav>
   </article>
