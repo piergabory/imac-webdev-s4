16907abb7dad07d097c71e64dfc80f901208c6a6
Pierre Gabory <piergabory@gmail.com>
2019-05-08 14:16:42 +0200

    Search view css

--- src/components/screens/search.screen.js
+++ src/components/screens/search.screen.js
@@ -3,12 +3,12 @@ import Card from '../card'
 
 export default ({state, actions, ignore, onSelection}) => {
   const ingnoredIds = ignore.map(hero => hero.id)
-  const visibleMatches = state.matches.filter(match => !ingnoredIds.includes(match.id))
+  const visibleMatches = state.matches.filter((match) => !ingnoredIds.includes(match.id)).filter((_, index) => index < 6)
   const matchedCard = (match, onclick) => <Card hero={match} decking={onSelection} onclick={onclick} deckingLabel='ADD'/>
 
   return <div className='searchwrapper'>
     <nav className='searchBox'>
-      <input oninput={ ev => actions.search(ev.target.value) } type='text' placeholder='Luke Skywalker'/>
+      <input className='searchField' oninput={ ev => actions.search(ev.target.value) } type='text' placeholder='Luke Skywalker'/>
     </nav>
     {
       (state.preview)
